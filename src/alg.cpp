f49ef0508b347e793c96df108ef465773a556daf
ErshovN <99174712+ErshovN@users.noreply.github.com>
2022-02-27 19:23:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,88 +4,63 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  int f = 0;
-  for (int i = 2; i * i <= value; i++) {
-      if (value % i == 0) {
-          f = 1;
-      }
-  }
-  if (f == 0) {
-      return true;
-  }
-  else {
-      return false;
-  }
+// вставьте код функции
+    int a, k, i;
+    k = 0;
+    a = value;
+    for (i = 1; i <= a; i++) {
+        if (a % i == 0) k = k + 1;
+    }
+    if (k != 2)
+        return false;
+    else
+        return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  uint64_t a = 1;
-  if (a == n) {
-      return 2;
-  }
-  else {
-      while (true)
-      {
-          for (uint64_t i = 3; ; i++) {
-              uint64_t f = 1;
-              for (uint64_t j = 2; j * j <= i; j++) {
-                  if ((i % j) == 0) {
-                      f = 0;
-                      break;
-                  }
-              }
-              if (f == 1) {
-                  a++;
-                  if (a == n) {
-                      return i;
-                  }
-              }
-          }
-      }
-
-  }
+// вставьте код функции
+    int count = 0;
+    int current = 0;
+    for (int i = 2; i < 10000000; i++) {
+        if (checkPrime(i) == true) {
+            count++;
+            if (count == n) {
+                current = i;
+                break;
+            }
+        }
+    }
+    return current;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  uint64_t v = value + 1;
-  while (true)
-  {
-      uint64_t f = 0;
-      for (uint64_t i = 2; i * i <= v; i++) {
-          if (v % i == 0) {
-              f = 1;
-          }
-      }
-      if (f == 0) {
-          return v;
-      }
-      else {
-          v++;
-      }
-  }
+// вставьте код функции
+    while (1) {
+        int c = 0;
+        value++;
+        if (!checkPrime(value)) {
+            c++;
+        }
+        if (c == 0) {
+            return value;
+            break;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t v = 0;
-    uint64_t s = 0;
-    while (v < hbound)
-    {
-        int f = 0;
-        for (int i = 2; i * i <= v; i++) {
-            if (v % i == 0) {
-                f = 1;
-            }
+// вставьте код функции
+    uint64_t sum = 5;
+    for (int i = 2; i < hbound; i++) {
+        if (i % 2 == 0) {
+            continue;
         }
-        if (v == 1) {
-            f = 1;
+        if (i % 3 == 0) {
+            continue;
         }
-        if (f == 0) {
-            s += v;
+        if (checkPrime(i)) {
+            sum = sum + i;
         }
-        v++;
     }
-    return s;
+    return sum;
 }
